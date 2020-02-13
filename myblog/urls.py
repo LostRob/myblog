ccd96ce6ae929754df28ae5ee2b8114d73362e74
Blog <1318995183@qq.com>
2020-02-14 00:40:29 +0800

    修复后台管理页面无样式

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path,include,re_path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -28,7 +30,7 @@ urlpatterns = [
     # path(r'',include('login.urls',namespace='login')),
     re_path(r'^search/', include('haystack.urls')),
     url(r'^accounts/', include('allauth.urls')),
-]
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
 
 
 
